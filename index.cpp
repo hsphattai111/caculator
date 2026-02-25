bdd79855148e4f790d819fa5a00a93916bfbcc3e
hsphattai111 <hoangsangnguyencong@gmail.com~>
2026-02-26 00:03:21 +0700

    caculator-version-demo

---
+++ index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+
+    cout << "Nhap so thu nhat: ";
+    cin >> a;
+    cout << "Nhap so thu hai: ";
+    cin >> b;
+
+    cout << "Tong: " << a + b << endl;
+    cout << "Hieu: " << a - b << endl;
+   
+    return 0;
+}
